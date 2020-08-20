70c542e91e4eb1c40944b39661a48bfbe592b0d0
Gabriel J <60169516+ecrseer@users.noreply.github.com>
2020-08-20 01:43:03 -0300

    recebendo json

---
+++ src/containers/main/Components/Board.jsx
@@ -0,0 +1,12 @@
+import React, {memo} from 'react';
+import PropTypes from 'prop-types';
+import {Grid,Skeleton} from '../'
+
+function Board ({data}){
+    const {cases,todayDeaths,recovered
+    ,deaths,todayCases}= data
+
+    return (
+        <div></div>
+    )
+}
