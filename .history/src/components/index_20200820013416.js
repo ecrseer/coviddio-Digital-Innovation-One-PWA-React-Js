aff2db59d83669f215bad2bbfcae0142fec84da7
Gabriel J <60169516+ecrseer@users.noreply.github.com>
2020-08-20 01:37:06 -0300

    components import

---
+++ .history/src/components/index_20200820013416.js
@@ -0,0 +1,7 @@
+import {Card, CardContent} from './Card'
+import Button from './Button'
+import Grid from './Grid'
+import Typography from './Typography'
+import Skeleton from './Skeleton'
+import Select from './Select'
+import MenuItem from './MenuItem'
