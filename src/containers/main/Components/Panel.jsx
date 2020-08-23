95baed89f5c50a7b0b6fa1d30a0969ca5efe6a98
Gabriel J <60169516+ecrseer@users.noreply.github.com>
2020-08-23 02:20:31 -0300

    created panel

---
+++ src/containers/main/Components/Panel.jsx
@@ -0,0 +1,9 @@
+import {Button,Select,MenuItem}
+ from '../../../components/';
+
+ import React,{memo} from 'react';
+ import RefreshIco from '../../../assets/refresh.svg';
+
+ function Panel({something}){
+   
+ }
