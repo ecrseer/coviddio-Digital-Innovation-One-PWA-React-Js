4796bb561cdd3eb34f034996194e6bc2d1cf2085
Gabriel J <60169516+ecrseer@users.noreply.github.com>
2020-08-23 02:22:57 -0300

    navigador from panel

--- src/containers/main/Components/Panel.jsx
+++ src/containers/main/Components/Panel.jsx
@@ -3,7 +3,11 @@ import {Button,Select,MenuItem}
 
  import React,{memo} from 'react';
  import RefreshIco from '../../../assets/refresh.svg';
+ import {CardEstilizado,ItemEstilizado} from './style'
+
+
+ const navigadorTemCompar = navigator.share;
 
  function Panel({something}){
-   
+
  }
