41fdbfad85b2177165407c2c1e5fea5116025e04
Gabriel J <60169516+ecrseer@users.noreply.github.com>
2020-08-25 15:36:21 -0300

    worker added

--- public/worker.js
+++ public/worker.js
@@ -1,6 +1,7 @@
 var CACHE_NAME = 'gjpwa';
 var urlsToCache = ['/'];
 
+//instala o servico no celular
 self.addEventListener('install',event=>{
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -9,5 +10,36 @@ self.addEventListener('install',event=>{
        return cache.addAll(urlsToCache);
      }
      )
+  );
+});
+
+//cache and return requests
+self.addEventListener('fetch',event=>{
+  event.respondWith(
+    caches.match(event.request)
+     .then(function(response){
+       if(response)
+        return response;
+
+        return fetch(event.request);
+     })
+  );
+});
+
+//atualiza o service worker
+self.addEventListener('activate',event=>{
+  var cacheWhiteList=['pwa-task-manager'];
+
+  event.waitUntil(
+    cache.keys().then(cacheNames=>{
+      return Promise.all(
+        cacheNames.map(cacheName=>{
+          if(cacheWhiteList.indexOf(cacheName)===-1)
+            return caches.delete(cacheName);
+
+
+        })
+      )
+    })
   );
 });
